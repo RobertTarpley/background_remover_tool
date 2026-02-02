b12c1d7f0d386e96697a5b12ce96ee1616a9e6a8
RobertTarpley <131493065+RobertTarpley@users.noreply.github.com>
2026-02-02 16:35:58 -0500

    try port swap 8080 main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import os
-import uvicorn
+from api.main import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
-    uvicorn.run("api.main:app", host="0.0.0.0", port=port)
+    import uvicorn
+    port = int(os.environ.get("PORT", "8080"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
